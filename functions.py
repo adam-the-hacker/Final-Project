f1334df601059725bee0b44562107b4a5d354a02
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-16 15:14:09 +0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pieces, tetris
 
+
 def rotation(piece):
     piece2 = [[0 for i in range(len(piece[0]))] for j in range(len(piece))]
     for x in range(len(piece)):
@@ -7,5 +8,19 @@ def rotation(piece):
             piece2[y][len(piece)-1-x] = piece[x][y]
     return piece2
 
-def colision(piece):
-    if main
+
+def mouvement(piece, direction):
+    npiece =                            
+
+
+
+'''       À CONTINUER APRES LA FONCTION DU MOUVEMENT DES PIÈCES
+
+def iscolision(piece, direction): # -1 pour la gauche 
+    for y in piece:
+        for x in y:
+            if piece[y][x] > 0 and maingrid[y][x+direction] == 1:
+                return True                                         # Renvoie vrai s'il y a une colision
+            elif piece[x][y] == 0:
+                continue
+'''                     
