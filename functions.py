b5b96b2eeb58890297ce69d9add28092a4dffbac
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-16 19:37:26 +0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 from pieces import *
 from tetris import maingrid
 
-
+# Testé et fonctionnel
 def rotation(piece):
     piece2 = [[0 for i in range(len(piece[0]))] for j in range(len(piece))]
     for x in range(len(piece)):
@@ -9,14 +9,14 @@ def rotation(piece):
             piece2[y][len(piece)-1-x] = piece[x][y]
     return piece2
 
-
+# A compléter puis tester
 def mouvement(piece, direction):             # -1 pour gauche et 1 pour droite
-    for i in piece:
-        for j in i:
+    for i in range(len(piece)):
+        for j in range(len(piece[1])):
             if piece[i][j] > 0:
                 npiece = piece[i][j]
-    for y in maingrid:
-        for x in y:
+    for y in range(len(maingrid)):
+        for x in range(len(maingrid[1])):
             if maingrid[y][x] == npiece:
                 if iscolision(piece, direction):
                     break
@@ -24,7 +24,7 @@ def mouvement(piece, direction):             # -1 pour gauche et 1 pour droite
                     maingrid[y][x] = maingrid[y][x+direction]
                     maingrid[y][x] = 0
 
-                  
+# Testé et fonctionnel
 def poser(piece):
     for i in range(len(piece)):
         for j in range(len(piece[1])):
