d5ecb64a7e803490d72326f29464eec0efbf91fc
ines <128018708+ines573@users.noreply.github.com>
2023-03-16 09:48:54 +0400

    Update pièces.py

--- pièces.py
+++ pièces.py
@@ -1,8 +1,34 @@
 ### LISTE DES PIÈCES
+# https://static.wikia.nocookie.net/tetrisconcept/images/c/ca/Tetromino_image.png/revision/latest?cb=20090706171943
 
-cyan=[[]
-      []
-      []
-      [],
+cyan=[[0,0,0,0],
+      [0,0,0,0],
+      [1,1,1,1],
+      [0,0,0,0]
      ]
-blue=[]
+
+blue=[[0,0,0],
+     [1,0,0],
+     [1,1,1]]
+
+orange=[[0,0,0],
+        [0,0,1],
+        [1,1,1]]
+
+yellow=[[1,1],
+       [1,1]]
+
+green=[[0,1,1],
+       [0,1,0],
+       [1,1,0]]
+
+purple=[[0,1,0],
+        [1,1,1],
+        [0,0,0]]
+
+red=[[1,1,0],
+     [0,1,0],
+     [0,1,1]]
+
+# rotation : (x,y) -> (y, (len(piece)-1)-x
+
