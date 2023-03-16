d5895833071c9cf9fe0a08c487ae768b38ff1075
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-16 10:43:16 +0400

    Update functions.py
    Rotation fonctonelle ~Adam

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 def rotation(piece):
-  piece2 = [[0 for i range(len(piece)[0])] for j in range(len(piece))]
-  for x in range(len(piece)):
-    for y in range(len(piece))
-      piece[x][y] = piece[y][x]
+    piece2 = [[0 for i in range(len(piece[0]))] for j in range(len(piece))]
+    for x in range(len(piece)):
+        for y in range(len(piece)):
+            piece2[y][len(piece)-1-x] = piece[x][y]
+    return piece2
