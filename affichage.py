b0e0f2f285cbc83352ac491c2b9eb4499ac3f02c
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-16 19:41:17 +0400

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -4,3 +4,9 @@ def showgrid():
   for y in range(len(maingrid)):
     for x in range(len(maingrid[1])):
       if maingrid[y][x] == 0:
+      '''
+      Si 0, afficher case noire
+      Si 1, afficher le bloc de la couleur correspondante
+      Si 2, même chose... ect
+      Si 9, afficher vide
+      '''
