ee0aed306a99c68c919cdbb3b20db77ad4e07f79
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-16 14:16:31 +0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
+import pieces, tetris
+
 def rotation(piece):
     piece2 = [[0 for i in range(len(piece[0]))] for j in range(len(piece))]
     for x in range(len(piece)):
         for y in range(len(piece)):
             piece2[y][len(piece)-1-x] = piece[x][y]
     return piece2
+
+def colision(piece):
+    if main
