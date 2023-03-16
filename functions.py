44107de572cedb3b8e0b6db044de22cae8999b60
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-16 09:49:29 +0400

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,5 @@
+def rotation(piece):
+  piece2 = [[0 for i range(len(piece)[0])] for j in range(len(piece))]
+  for x in range(len(piece)):
+    for y in range(len(piece))
+      piece[x][y] = piece[y][x]
