72b46e30141102eb8590c1db55ce3627dbe4df77
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-17 21:01:01 +0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -6,29 +6,28 @@ from random import*
 from functions import *
 from pieces import *
 
-maingrid = [[9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9], 
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9]]
+maingrid = [[9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 8, 8, 8, 8, 8]]
 
 init()
-
 blocblanc = image.load('Bloc blanc.PNG')
 bloccyan = image.load('Bloc cyan.PNG')
 blocblue = image.load('Bloc blue.PNG')
@@ -37,13 +36,19 @@ blocyellow = image.load('Bloc yellow.PNG')
 blocgreen = image.load('Bloc green.PNG')
 blocpurple = image.load('Bloc purple.PNG')
 blocred = image.load('Bloc red.PNG')
+blocscore = image.load('Bloc score.PNG')
 
-fenetre = display.set_mode((300,600), RESIZABLE)  # Obligation sur la taille : 2x = y
+fenetre = display.set_mode((480,600), RESIZABLE)  # Obligation sur la taille : 2x = y
                                                   # Si vous voulez changer la taille de l'interface,
                                                   # changez la taille de tous les carrés telle que pour
                                                   # chaque carré, x(interface) = 10*x(carré)
                                                   # et un coef de 20 pour les y
 
-while True:
+
+continuer = 1
+while continuer == 1:
+    for evenements in event.get():
+        if evenements.type == QUIT:
+            continuer = 0
     showgrid()
     display.flip()
