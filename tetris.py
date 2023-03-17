9f0d04e2256bb5393b9bdc6054b202044366c95d
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-18 00:39:27 +0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -44,11 +44,15 @@ fenetre = display.set_mode((480,600), RESIZABLE)  # Obligation sur la taille : 2
                                                   # chaque carré, x(interface) = 10*x(carré)
                                                   # et un coef de 20 pour les y
 
-
+placedbloc = 1 # Si cette variable est égale à 1, alors on appelle la fonction poser(bloc) pour générer un bloc aléatoire
+               # Si elle est égale à 0, on attend que le bloc soit posé pour la mettre à 1 et donc générer un nouveau bloc
 continuer = 1
 while continuer == 1:
     for evenements in event.get():
         if evenements.type == QUIT:
             continuer = 0
+    if placedbloc == 1:
+            poser(bloc) # Cela ne marchera qu'une fois tous les fichiers mis ensemble dans un seul
+            placedbloc = 0
     showgrid()
     display.flip()
