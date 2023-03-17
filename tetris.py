fe173aa9a717ab7b8e1e03bd73bf6138058531af
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-17 17:04:06 +0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -29,5 +29,21 @@ maingrid = [[9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
 
 init()
 
-''' L'interface est en 537 par 1067, mais je peux changer ''' 
-fenetre = display.set_mode((537, 1067), RESIZABLE) 
+blocblanc = image.load('Bloc blanc.PNG')
+bloccyan = image.load('Bloc cyan.PNG')
+blocblue = image.load('Bloc blue.PNG')
+blocorange = image.load('Bloc orange.PNG')
+blocyellow = image.load('Bloc yellow.PNG')
+blocgreen = image.load('Bloc green.PNG')
+blocpurple = image.load('Bloc purple.PNG')
+blocred = image.load('Bloc red.PNG')
+
+fenetre = display.set_mode((300,600), RESIZABLE)  # Obligation sur la taille : 2x = y
+                                                  # Si vous voulez changer la taille de l'interface,
+                                                  # changez la taille de tous les carrés telle que pour
+                                                  # chaque carré, x(interface) = 10*x(carré)
+                                                  # et un coef de 20 pour les y
+
+while True:
+    showgrid()
+    display.flip()
