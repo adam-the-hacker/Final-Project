0bd31e2cc46062843020bfba1397689942266b5b
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-17 20:59:17 +0400

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -3,10 +3,21 @@ import Tetris
 def showgrid():
   for y in range(len(maingrid)):
     for x in range(len(maingrid[1])):
-      if maingrid[y][x] == 0:
-      '''
-      Si 0, afficher case noire
-      Si 1, afficher le bloc de la couleur correspondante
-      Si 2, même chose... ect
-      Si 9, afficher vide
-      '''
+        if maingrid[y][x] == 0:
+            fenetre.blit(blocblanc, ((x-1)*30,y*30)) # Le coef 30 existe car ce sont les dimensions d'un carré
+        if maingrid[y][x] == 1:
+            fenetre.blit(bloccyan, ((x-1)*30,y*30))
+        if maingrid[y][x] == 2:
+            fenetre.blit(blocblue, ((x-1)*30,y*30))
+        if maingrid[y][x] == 3:
+            fenetre.blit(blocorange, ((x-1)*30,y*30))
+        if maingrid[y][x] == 4:
+            fenetre.blit(blocyellow, ((x-1)*30,y*30))
+        if maingrid[y][x] == 5:
+            fenetre.blit(blocgreen, ((x-1)*30,y*30))
+        if maingrid[y][x] == 6:
+            fenetre.blit(blocpurple, ((x-1)*30,y*30))
+        if maingrid[y][x] == 7:
+            fenetre.blit(blocred, ((x-1)*30,y*30))
+        if maingrid[y][x] == 8:
+            fenetre.blit(blocscore, ((x-1)*30,y*30))
