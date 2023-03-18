b812ad4d36e6d2c064d10a9f2b3831a3935b1618
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-18 13:24:26 +0400

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,10 +1,12 @@
 ### LISTE DES PIÈCES
 # https://static.wikia.nocookie.net/tetrisconcept/images/c/ca/Tetromino_image.png/revision/latest?cb=20090706171943
 
-cyan=[[1,1,1,1],
+
+
+cyan=[[0,0,0,0],
       [0,0,0,0],
       [0,0,0,0],
-      [0,0,0,0]]
+      [1,1,1,1]]
 
 blue=[[2,0,0],
      [2,2,2],
@@ -28,4 +30,3 @@ purple=[[0,6,0],
 red=[[7,7,0],
      [0,7,7],
      [0,0,0]]
-
