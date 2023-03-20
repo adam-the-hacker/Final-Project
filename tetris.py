55f589910f01f0cdbc1bd1dea8cf90c845fdb6aa
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-20 23:21:34 +0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -44,15 +44,31 @@ fenetre = display.set_mode((480,600), RESIZABLE)  # Obligation sur la taille : 2
                                                   # chaque carré, x(interface) = 10*x(carré)
                                                   # et un coef de 20 pour les y
 
-placedbloc = 1 # Si cette variable est égale à 1, alors on appelle la fonction poser(bloc) pour générer un bloc aléatoire
-               # Si elle est égale à 0, on attend que le bloc soit posé pour la mettre à 1 et donc générer un nouveau bloc
+                                                
+isactivepiece = 0       # Si cette variable est égale à 1, alors on appelle la fonction poser(bloc) pour générer un bloc aléatoire
+                        # Si elle est égale à 0, on attend que le bloc soit posé pour la mettre à 1 et donc générer un nouveau bloc
+deplacement_y = 1       # On l'augmentera selon la difficulté
+piece_x = 0
+piece_y = 0
+comp = 0
 continuer = 1
+
+
 while continuer == 1:
+    time.Clock().tick(200)
     for evenements in event.get():
         if evenements.type == QUIT:
             continuer = 0
-    if placedbloc == 1:
-            poser(bloc) # Cela ne marchera qu'une fois tous les fichiers mis ensemble dans un seul
-            placedbloc = 0
+    if isactivepiece == 0:
+        poser(randombloc(), 3, -1)
+        isactivepiece = 1
+
+    # Descente du bloc 
+    if comp % (1.5*100) == 0:             # Temps en secs entre chaque descente du bloc (qu'on multiplie par 100) (on néglige quelques milisecondes de décalage)
+        deplacer_piece(activebloc, piece_x, piece_y + deplacement_y)
+
+
     showgrid()
     display.flip()
+    comp += 1
+
