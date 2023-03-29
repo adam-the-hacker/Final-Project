afe9252ab9e03f742bf035cedb7dcac3c136b4be
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-03-29 14:12:20 +0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -37,38 +37,45 @@ blocgreen = image.load('Bloc green.PNG')
 blocpurple = image.load('Bloc purple.PNG')
 blocred = image.load('Bloc red.PNG')
 blocscore = image.load('Bloc score.PNG')
+fenetre = display.set_mode((480,600), RESIZABLE)
 
-fenetre = display.set_mode((480,600), RESIZABLE)  # Obligation sur la taille : 2x = y
-                                                  # Si vous voulez changer la taille de l'interface,
-                                                  # changez la taille de tous les carrés telle que pour
-                                                  # chaque carré, x(interface) = 10*x(carré)
-                                                  # et un coef de 20 pour les y
+
 
                                                 
-isactivepiece = 0       # Si cette variable est égale à 1, alors on appelle la fonction poser(bloc) pour générer un bloc aléatoire
-                        # Si elle est égale à 0, on attend que le bloc soit posé pour la mettre à 1 et donc générer un nouveau bloc
-deplacement_y = 1       # On l'augmentera selon la difficulté
+isactivepiece = 0 # Si cette variable est égale à 1, alors on appelle la fonction poser(bloc) pour générer un bloc aléatoire
+                  # Si elle est égale à 0, on attend que le bloc soit posé pour la mettre à 1 et donc générer un nouveau bloc
 piece_x = 0
 piece_y = 0
 comp = 0
 continuer = 1
+vspeed = 10         # Vertical speed, On le réduira pour augmenter la difficulté
+temps = t.time()
+
 
 
 while continuer == 1:
-    time.Clock().tick(200)
+    time.Clock().tick(10)
     for evenements in event.get():
         if evenements.type == QUIT:
             continuer = 0
+            
     if isactivepiece == 0:
         poser(randombloc(), 3, -1)
         isactivepiece = 1
 
-    # Descente du bloc 
-    if comp % (1.5*100) == 0:             # Temps en secs entre chaque descente du bloc (qu'on multiplie par 100) (on néglige quelques milisecondes de décalage)
-        deplacer_piece(activebloc, piece_x, piece_y + deplacement_y)
+    keyb = key.get_pressed()
+    if keyb[K_RIGHT] and piece_x + 1 < 11 - len(activebloc[0]):
+        deplacer_piece(activebloc, piece_x + 1, piece_y)
 
+    if keyb[K_LEFT] and piece_x > 0:
+        deplacer_piece(activebloc, piece_x - 1, piece_y)
+
+    if keyb[K_SPACE]:
+        continue
+
+    if comp % (vspeed) == 0:
+        deplacer_piece(activebloc, piece_x, piece_y + 1)
 
     showgrid()
     display.flip()
     comp += 1
-
