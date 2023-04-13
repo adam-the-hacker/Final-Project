3767171fbc66e55031e6520207a5e3df85e8e1d3
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-04-13 09:11:05 +0400

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -1,5 +1,3 @@
-import Tetris
-
 
 # Testé et fonctionnel
 def showgrid():
@@ -7,19 +5,19 @@ def showgrid():
     for x in range(len(maingrid[1])):
         if maingrid[y][x] == 0:
             fenetre.blit(blocblanc, ((x-1)*30,y*30)) # Le coef 30 existe car ce sont les dimensions d'un carré
-        if maingrid[y][x] == 1:
+        if maingrid[y][x] == 1 or maingrid[y][x] == 11:
             fenetre.blit(bloccyan, ((x-1)*30,y*30))
-        if maingrid[y][x] == 2:
+        if maingrid[y][x] == 2 or maingrid[y][x] == 22:
             fenetre.blit(blocblue, ((x-1)*30,y*30))
-        if maingrid[y][x] == 3:
+        if maingrid[y][x] == 3 or maingrid[y][x] == 33:
             fenetre.blit(blocorange, ((x-1)*30,y*30))
-        if maingrid[y][x] == 4:
+        if maingrid[y][x] == 4 or maingrid[y][x] == 44:
             fenetre.blit(blocyellow, ((x-1)*30,y*30))
-        if maingrid[y][x] == 5:
+        if maingrid[y][x] == 5 or maingrid[y][x] == 55:
             fenetre.blit(blocgreen, ((x-1)*30,y*30))
-        if maingrid[y][x] == 6:
+        if maingrid[y][x] == 6 or maingrid[y][x] == 66:
             fenetre.blit(blocpurple, ((x-1)*30,y*30))
-        if maingrid[y][x] == 7:
+        if maingrid[y][x] == 7 or maingrid[y][x] == 77:
             fenetre.blit(blocred, ((x-1)*30,y*30))
         if maingrid[y][x] == 8:
             fenetre.blit(blocscore, ((x-1)*30,y*30))
