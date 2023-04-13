b725bcc00f5e5671920f8cc0fbf820be388fff0f
ines <128018708+ines573@users.noreply.github.com>
2023-04-13 09:22:12 +0400

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -2,6 +2,7 @@
 # https://static.wikia.nocookie.net/tetrisconcept/images/c/ca/Tetromino_image.png/revision/latest?cb=20090706171943
 
 cyan=[[0,0,0,0],
+      [0,0,0,0],
       [1,1,1,1],
       [0,0,0,0]]
 
@@ -27,27 +28,3 @@ purple=[[0,6,0],
 red=[[7,7,0],
      [0,7,7],
      [0,0,0]]
-
-//
-
-
-
-# cyan= [[1,1,1,1]]
-
-#blue=[[2,0,0],
-  #   [2,2,2]]
-
-#orange=[[0,0,3],
-  #      [3,3,3]]
-
-#yellow=[[4,4],
-#        [4,4]]
-
-#green=[[0,5,5],
-#       [5,5,0]]
-
-#purple=[[0,6,0],
- #       [6,6,6]]
-
-#red=[[7,7,0],
-#     [0,7,7]]
