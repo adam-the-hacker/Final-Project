97cc0339f5f8f66208ba715eaa2d13a74fcc2ee1
adam-the-hacker <74697220+adam-the-hacker@users.noreply.github.com>
2023-04-13 09:38:38 +0400

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -21,3 +21,43 @@ def showgrid():
             fenetre.blit(blocred, ((x-1)*30,y*30))
         if maingrid[y][x] == 8:
             fenetre.blit(blocscore, ((x-1)*30,y*30))
+
+
+def hardsetbloc(piece):
+    global piece_x, piece_y
+
+    # Récupérer le nombre de la pièce
+    npiece = 0
+    for z in range(len(piece)):
+        for v in range(len(piece[0])):
+            if piece[z][v] != 0:
+                npiece = piece[z][v]
+
+    # Doubler ce nombre
+    nblocdur = int(str(npiece) + str(npiece))
+
+    # Effacer
+    piece = rotate_piece(piece, (rotated_bloc % 4) - 1)
+
+    for i in range(len(piece)):
+        for j in range(len(piece[0])):
+            if piece[i][j] == npiece:
+                maingrid[i + piece_y][j + piece_x + 1] = 0
+
+    piece = rotate_piece(piece, 1)
+
+    for i in range(len(piece)):
+        for j in range(len(piece[0])):
+            if piece[i][j] == npiece:
+                maingrid[i + piece_y][j + piece_x + 1] = 0
+
+    # Générer la matrice du bloc dur
+    piecedur = piece
+    for x in range(len(piecedur)):
+        for y in range(len(piecedur[0])):
+            if piecedur[x][y] == npiece:
+                piecedur[x][y] = nblocdur
+
+    # Poser en dur
+
+    poser(piecedur, piece_x, piece_y)
