b47d30a5a8f3448325a2da08c7a45c162b9440ea
ines <128018708+ines573@users.noreply.github.com>
2023-04-30 17:50:03 +0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -1,66 +1,3 @@
-# Main code
-import random
-import time as t
-from pygame import*
-from math import*
-from random import*
-
-
-maingrid = [[9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8],
-            [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-            [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-            [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]]
-
-
-init()
-blocblanc = image.load('Bloc blanc.PNG')
-bloccyan = image.load('Bloc cyan.PNG')
-blocblue = image.load('Bloc blue.PNG')
-blocorange = image.load('Bloc orange.PNG')
-blocyellow = image.load('Bloc yellow.PNG')
-blocgreen = image.load('Bloc green.PNG')
-blocpurple = image.load('Bloc purple.PNG')
-blocred = image.load('Bloc red.PNG')
-blocscore = image.load('Bloc score.PNG')
-
-fenetre = display.set_mode((480,600), RESIZABLE)
-
-isactivepiece = 0 # Si cette variable est égale à 1, alors on appelle la fonction poser(bloc) pour générer un bloc aléatoire
-                        # Si elle est égale à 0, on attend que le bloc soit posé pour la mettre à 1 et donc générer un nouveau bloc
-piece_x = 0
-piece_y = 0
-comp = 0
-continuer = 1
-vspeed = 10         # Vertical speed, On le réduira pour augmenter la difficulté
-level = vspeed // 10
-temps = t.time()
-rotated_bloc = 0
-piece0 = True
-activebloc = None
-score = 0
-totallines = 0
-
-
-
 while continuer == 1:
     time.Clock().tick(10)
     for evenements in event.get():
@@ -88,7 +25,10 @@ while continuer == 1:
             piece_x +=1
 
     if keyb[K_SPACE]:
-        continue
+        while not collision(activebloc):
+            piece_y += 1
+        piece_y -=1
+        score+=10 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!??
 
     if keyb[K_DOWN]:
         piece_y += 1
@@ -119,3 +59,9 @@ while continuer == 1:
     display.flip()
     comp += 1
 
+#On met le score dans un doc
+high_scores.write(str(score)+"\n")
+high_scores.close()
+# Syntaxes :
+# Lire le fichier: open("score.txt","r")
+# Le lire ligne par ligne: high_scores_list = high_scores.readlines.
